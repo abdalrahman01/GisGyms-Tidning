f51eda7807cfebaef5faee16042dcb99df3bbb84
Abd Alrahman Atieh <abdalrahman.atieh@gmail.com>
2021-04-17 13:58:59 +0200

    added home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,21 +1,15 @@
-import { Component } from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches),
-      shareReplay()
-    );
+  constructor() { }
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  ngOnInit(): void {
+  }
 
 }
