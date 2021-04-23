e14ec554442477cfd10fc17d088cbd1292d00aae
Abd Alrahman Atieh <abdalrahman.atieh@gmail.com>
2021-04-23 09:39:31 +0200

    added a json file for news articles

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import news from "../../assets/news/news.json";
 
 @Component({
   selector: 'app-home',
@@ -9,7 +10,13 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
+
+  News: any = news;
+
   ngOnInit(): void {
   }
+
+
+  
 
 }
